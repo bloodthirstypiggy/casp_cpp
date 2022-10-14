79692925717259ff7f9c3b42ac086385928a003e
bloodthirstypiggy <piggybloodthirsty@gmail.com>
2022-10-14 10:19:12 +0300

    big  update!

--- client.cpp
+++ client.cpp
@@ -6,9 +6,22 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <iostream>
-#define BUFFER_SIZE 100
+#define BUFFER_SIZE 10000
 
 //you shoud export PORT=20018 on linux machine
+
+int writestr(int sock, const char *str)
+{
+    int n, len = strlen(str);
+    while (len > 0)
+    {
+        n = write(sock, str, len);
+        if (n < 0) return n;
+        str += n;
+        len -= n;
+    }
+    return 0;
+}
 
 int main(int argc, char const* argv[])
 {
@@ -29,16 +42,17 @@ int main(int argc, char const* argv[])
 
 	//connect
 
-	char buffer[BUFFER_SIZE] = "start";
-	char message[] = "hui hui hui";
-	char* end = "stop";
 	int client_fd = connect(conSocket,reinterpret_cast<struct sockaddr*>(&client_adress), addrlen);
-	send(conSocket, buffer, BUFFER_SIZE, 0);
-	send(conSocket, message, BUFFER_SIZE, 0);
-	//send(conSocket, bye, strlen(bye), 0);
-	//char* end = "###";
-	send(conSocket, end, BUFFER_SIZE, 0);
-	std::cout << "hello message sent! from clientcpp" << std::endl;
+	int n;
+	n = writestr(conSocket, "start\n");
+	n = writestr(conSocket, "message\n");
+	n = writestr(conSocket, "message1\n");
+	n = writestr(conSocket, "message2\n");
+	n = writestr(conSocket, "end\n");
+	//char* recev = (char*)malloc(100);
+	//int recv1 = recv(conSocket, recev, sizeof(recev), 0);
+	//std::cout << recv1;
+
 
 	return 0;
 }
